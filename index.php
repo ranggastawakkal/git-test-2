e22ff00fe0bd9d5a6c61906b8ed5d6922d9c0c4f
ranggastawakkal <rangga.awantara@gmail.com>
2023-02-02 15:52:14 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae cupiditate dolore accusamus commodi minima dolorem ea. Fuga odit sed a!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
